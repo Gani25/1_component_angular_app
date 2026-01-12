0d9bf1afdf3d71315141292b29f78ae744490bb5
Gani25 <memonabdulgani3d@gmail.com>
2026-01-12 19:32:18 +0530

    String interpolation and Property Binding

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-
+  // property of a class/field
+  selectedUser = DUMMY_USERS[randomIndex];
 }
