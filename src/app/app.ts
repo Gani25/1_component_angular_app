f4ea69df5a4a2cc44663a81952c8ec6c9fe4ccd6
Gani25 <memonabdulgani3d@gmail.com>
2026-01-12 17:58:02 +0530

    Creating Components in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Header } from './header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('1_component_angular_app');
